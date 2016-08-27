e534a0a92b75c1934e48e615a5150db3761b0e8a
Krishna Kandula <krishna.c.kandula@gmail.com>
2016-08-27 17:37:34 -0500

    Added view pager to layout

---
+++ app/src/main/java/com/silver/krish/schedulr/MainActivity.java
@@ -0,0 +1,15 @@
+package com.silver.krish.schedulr;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
